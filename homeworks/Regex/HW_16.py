76219b118cd9e14a13a8c3491ab4bfdf0de60c81
zahrebelnyi_vitalii <zfdhhfhh@gmail.com>
2022-07-23 11:50:36 +0300

    HW_16

--- homeworks/Regex/HW_16.py
+++ homeworks/Regex/HW_16.py
@@ -11,20 +11,23 @@ print("=" * 50)
 
 pattern = r"[\wd\.]{1,255}@\w+\.+[\wd\.]{1,255}"
 text = 'zagrebelnyivitalii@gmail.com'
-result = re.match(pattern=pattern, string=text)
+result = re.findall(pattern=pattern, string=text)
 
-print(result.groups())
-
-print("=" * 50)
-
-pattern = r"\.[0]*"
-text = "216.008.094.190"
-result = re.sub(pattern=pattern, repl=" ", string=text)
 print(result)
 
 print("=" * 50)
 
-pattern = r"\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,2}"
+pattern = r"([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])[.]([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])[.]" \
+          r"([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])[.]([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])"
+text = '000.000.000.000'
+
+regex = re.findall(pattern=pattern, string=text)
+regex2 = ', '.join('.'.join(tup) for tup in regex)
+ip_address = re.sub(r'\b0+(\d)', r'\1', regex2)
+print(ip_address)
+print("=" * 50)
+
+pattern = r'^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$'
 text = "153.192.238.84"
 result = re.match(pattern=pattern, string=text)
 
